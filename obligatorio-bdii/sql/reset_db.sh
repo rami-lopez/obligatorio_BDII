ad78f7fc82e849556b710f2f6fea82d2d8f0bed1
Ramiro López Paz <RAMIRO.LOPEZ@correo.ucu.edu.uy>
2026-06-01 18:55:20 -0300

    pasaje 3 a MySQL

--- obligatorio-bdii/sql/reset_db.sh
+++ obligatorio-bdii/sql/reset_db.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DB_NAME="obligatorio_bdii"
-DB_USER="postgres"
+DB_USER="root"
 
 echo "Recreando base de datos $DB_NAME..."
-psql -U $DB_USER -c "DROP DATABASE IF EXISTS $DB_NAME;"
-psql -U $DB_USER -c "CREATE DATABASE $DB_NAME;"
-psql -U $DB_USER -d $DB_NAME -f 01_schema.sql
-psql -U $DB_USER -d $DB_NAME -f 02_inserts.sql
-echo "Listo."
+mysql -u $DB_USER -p -e "DROP DATABASE IF EXISTS $DB_NAME;"
+mysql -u $DB_USER -p -e "CREATE DATABASE $DB_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+mysql -u $DB_USER -p $DB_NAME < 01_schema.sql
+mysql -u $DB_USER -p $DB_NAME < 02_inserts.sql
+echo "Listo"
